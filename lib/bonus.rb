2fe908c5b432dd57d4fe72b6588a65e9fdbed726
Yusuf Celep <yusufcelep8@gmail.com>
2020-01-31 20:20:22 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -20,11 +20,8 @@ def bonus
    }
   }
 
-epic_tragedy[:montague][:hero][:status] = "dead"
-epic_tragedy[:capulet][:heroine][:status] = "dead"
+  epic_tragedy[:montague][:hero][:status] = "dead"
+  epic_tragedy[:capulet][:heroine][:status] = "dead"
 
-  
-
-  #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
   epic_tragedy
 end
